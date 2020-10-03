d891d74678b5595dda05aef70fb51f0ae3e779af
Mert Senel <mertsenel@gmail.com>
2020-10-03 18:35:32 +1000

    get the workflow working

--- New-AzAksLabPrerequisites.ps1
+++ New-AzAksLabPrerequisites.ps1
@@ -1,26 +1,24 @@
 <#
-    This Script is required to setup prerequisites in your Azure Subscription
-    The Script will Generate Resource Group of your choosing in your desired Azure Region
-    And then it generate a Service Principial That only has Access to that resource Group, you can feed into you Github Actions
-    Altough Secrets are protected, I still won't use a Subscription Level RBAC assignment, it's way too dangerous for the convinience it provides. 
+    This Script will generate a Service Principial to be used in Azure Login Github Action as a Secret 
 #>
-
-#Requires -Module @{ ModuleName = 'Az'; ModuleVersion = '4.7.0' }
-
-#Get the Local Config File
-$localConfigPath = "./local.settings.json"
-$config = Get-Content -Path $localConfigPath | ConvertFrom-Json -Depth 10
-
-#Makes Sure You are in the correct Azure Subscription
-Set-AzContext -Subscription $config.Az_SubscriptionID
-
-#Creates a New Resource Group
-$ResourceGroup = New-AzResourceGroup -Name $config.Az_ResourceGroupName -Location $config.Az_Location -Force
-
-$SpnName = $config.Az_SpnName
-
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $SpnName="azure-k8s-lab-ga-spn",
+    # Parameter help description
+    [Parameter()]
+    [string]
+    $EnvironmentVariableName = 'AZURE_CREDENTIALS'
+)
 # Create Service Principal with Az Cli as it already generates a Secret and able to print out result in SDK expected format
 # See this link for more information https://github.com/marketplace/actions/azure-login
-$SPNCreds = az ad sp create-for-rbac --name $SpnName --sdk-auth --role contributor --scopes $ResourceGroup.ResourceId
+$SPNCreds = az ad sp create-for-rbac --name $SpnName --sdk-auth --role contributor
 
-$SPNCreds | Set-Content -Path './local.spn.creds.json' -Force
+# Save the SPN Credentials to a local file so you can use it to create your Github Repository Secret
+$SPNCreds | Set-Content -Path "./$EnvironmentVariableName.json" -Force
+
+# We will also save it as an environment variable
+$LocalEnvVar = $SPNCreds | ConvertFrom-Json | ConvertTo-Json -Compress
+Set-Item -Path "Env:\$EnvironmentVariableName" -Value $LocalEnvVar
+
--- deploy-aks.ps1
+++ deploy-aks.ps1
@@ -1,20 +1,47 @@
 [CmdletBinding()]
 param (
-    # Parameter help description
-    [Parameter(Mandatory=$false)]
-    [string]
-    $settingsFilePath='settings.json'
-)
+    # Name of the Environment Variable Script will get the Azure SPN Credentials
+    [Parameter()][string]$EnvironmentVariableName = 'AZURE_CREDENTIALS',
+    [Parameter()][string]$ArmTemplateFilePath = './arm/aks.json',
+    [Parameter()][string]$ArmTemplateParameterFilePath = './arm/aks.parameters.json'
+    )
 
-$localConfigPath = "./local.settings.json"
-$config = Get-Content -Path $localConfigPath | ConvertFrom-Json -Depth 10
+$localSpnCreds = Get-ChildItem -Path "Env:\$EnvironmentVariableName"
+$spn = $localSpnCreds.Value | ConvertFrom-Json
 
+$ArmTemplateParameters = Get-Content -Path $ArmTemplateParameterFilePath | ConvertFrom-Json -Depth 10
+$config = $ArmTemplateParameters.parameters.config.value
 
-Import-Module Az.Aks
+#region Curate Variables
+$SubscriptionId = $spn.subscriptionId
+$ResourceGroupName = $config.project + '-' + $config.env + '-' + 'aks' + '-' + $config.region + $config.num
+$Location = $ArmTemplateParameters.parameters.location.value
+#endregion
 
-AksArgs = @{
-    ResourceGroupName = $config.Az_ResourceGroupName
-    
+#region Connect To Azure if Not connected Already
+$CurrentContext = Get-AzContext
+
+if ((!$CurrentContext) -or ($CurrentContext.Subscription.Id -ne $SubscriptionId)) {
+    [string]$clientId = $spn.clientId
+    [string]$clientSecret = $spn.clientSecret
+    # Convert to SecureString
+    [securestring]$secClientSecret = ConvertTo-SecureString $clientSecret -AsPlainText -Force
+    [pscredential]$spnCreds = New-Object System.Management.Automation.PSCredential ($clientId, $secClientSecret)
+    Connect-AzAccount -ServicePrincipal -Credential $spnCreds -Tenant $spn.tenantId -Scope Process | out-null
+    Set-AzContext -Subscription $SubscriptionId | Out-Null
+}
+#endregion
+
+#Create/Update the resource Group
+New-AzResourceGroup -ResourceGroupName $ResourceGroupName -Location $Location -Force | out-null
+
+$Args = @{
+    ResourceGroupName         = $ResourceGroupName
+    Name                      = "$(new-guid)"
+    TemplateFile              = $ArmTemplateFilePath
+    TemplateParameterFile     =  $ArmTemplateParameterFilePath
 }
 
-New-AzAksCluster 
+$AzDeployment = New-AzResourceGroupDeployment @Args
+
+$AzDeployment.Outputs
