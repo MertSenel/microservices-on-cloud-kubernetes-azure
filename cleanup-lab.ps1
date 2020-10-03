0c6a0e342d81888e4f097d5ee49ec9095958c634
Mert Senel <mertsenel@gmail.com>
2020-10-04 00:59:04 +1000

    need rg name as well

--- cleanup-lab.ps1
+++ cleanup-lab.ps1
@@ -10,6 +10,8 @@ $spn = $localSpnCreds.Value | ConvertFrom-Json
 $ArmTemplateParameters = Get-Content -Path $ArmTemplateParameterFilePath | ConvertFrom-Json -Depth 10
 $config = $ArmTemplateParameters.parameters.config.value
 
+$ResourceGroupName = $config.project + '-' + $config.env + '-' + 'aks' + '-' + $config.region + $config.num
+
 #region Connect To Azure if Not connected Already
 $CurrentContext = Get-AzContext
 
